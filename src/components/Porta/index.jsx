04e5b9feba402d0b81f064818ebd4e70bd6ab77c
bezerrarodrigo <bezerra.rodrigo@gmail.com>
2021-09-23 16:48:26 -0300

    feature: componente porta

---
+++ src/components/Porta/index.jsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.css'
+
+export default function Porta(props) {
+  return (
+    <div className={styles.area}>
+      <div className={styles.estrutura}>
+        <div className={styles.porta}>
+          <div className={styles.numero}>3</div>
+        </div>
+      </div>
+      <div className={styles.piso}>
+</div>
+    </div>
+  )
+}
